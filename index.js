4bbfe98bfb4fae7a36b4f0d71045445566177d7f
renkui <371397938@qq.com>
2019-10-18 12:48:35 +0800

    import

---
+++ index.js
@@ -0,0 +1,10 @@
+const url = require('url');
+const lmsdk_eth_providers = require('lmsdk-eth-providers');
+const Web3 = require('web3');
+
+var localURL = url.parse(location.href, true);
+
+if ( localURL.query._lmt === "ethereum" ) {
+    window.ethereum = new lmsdk_eth_providers.HttpProvider(localURL.query._lmv);
+    window.web3 = new Web3(window.ethereum);
+}
