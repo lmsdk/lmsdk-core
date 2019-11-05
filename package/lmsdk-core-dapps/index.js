4ddee790f5dc58b2c0d9eb77e52d27b7f41a971f
oblivioned <371397938@qq.com>
2019-11-05 15:42:41 +0800

    Update index.js

--- package/lmsdk-core-dapps/index.js
+++ package/lmsdk-core-dapps/index.js
@@ -1,7 +1,20 @@
+import LMPUtils from '../lmsdk-core-utils'
 import dapps_ethereum from './lmsdk-core-dapps-ethereum.js'
 
 var LMPDapps = function LMPDapps() {
     this.ethereum = new dapps_ethereum();
+}
+
+LMPDapps.prototype.openDappWithHttp = function(url) {
+    
+    var buff = new Buffer(url)
+    var schemasString = "limowallet://limowallet.org/dapps/open?type=http&url=" + buff.toString("base64");
+    
+    return new Promise(function(resolve, reject) {
+        if (LMPUtils.inLmApp) {
+            plus.bridge.exec("LMDapps", "openDappWithSchemas", [plus.bridge.callbackId(resolve, reject), schemasString])
+        }
+    });
 }
 
 module.exports = LMPDapps;
