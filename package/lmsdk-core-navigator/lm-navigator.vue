9261503a06f7055d83107a09d3745916e921418c
General <40221340+wangGeneral@users.noreply.github.com>
2019-11-06 10:52:41 +0800

    update navigator jump mode

--- package/lmsdk-core-navigator/lm-navigator.vue
+++ package/lmsdk-core-navigator/lm-navigator.vue
@@ -18,6 +18,11 @@
         },
         methods: {
             navigatorTagOnClick() {
+				if (typeof lmAPP === undefined) {
+					uni.navigateTo({
+						url: this.param
+					})
+				}
                 switch (this.type) {
                     case navigator.types.Push: return navigator.push(this.param).then()
                     case navigator.types.Pop:  return navigator.pop(this.param).then()
