5d2aed3c76003a7ba4c62cca41a9d4593f76268e
oblivioned <371397938@qq.com>
2019-11-06 18:02:17 +0800

    Update index.js

--- package/lmsdk-core-utils/index.js
+++ package/lmsdk-core-utils/index.js
@@ -1,9 +1,7 @@
-const url = require('url');
-const localURL = url.parse(location.href.replace('/#', ''), true);
 var LMPUtils = {}
 
-LMPUtils.inLmApp = localURL.query._lm ? true : false;
-LMPUtils.lmt = localURL.query._lmt;
-LMPUtils.lmv = localURL.query._lmv;
+LMPUtils.inLmApp = window.lmdapp ? true : undefined;
+LMPUtils.lmt = window.lmdapp.lmt;
+LMPUtils.lmv = window.lmdapp.lmv;
 
 module.exports = LMPUtils;
