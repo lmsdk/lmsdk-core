2c513880fbc6d49fc144d48d3890750e55238ba1
oblivioned <371397938@qq.com>
2019-11-07 10:26:49 +0800

    Update index.js

--- package/lmsdk-core-utils/index.js
+++ package/lmsdk-core-utils/index.js
@@ -1,7 +1,9 @@
 var LMPUtils = {}
 
-LMPUtils.inLmApp = window.lmdapp ? true : undefined;
-LMPUtils.lmt = window.lmdapp.lmt;
-LMPUtils.lmv = window.lmdapp.lmv;
+if (window.lmdapp) {
+    LMPUtils.inLmApp = true;
+    LMPUtils.lmt = window.lmdapp.lmt;
+    LMPUtils.lmv = window.lmdapp.lmv;    
+}
 
 module.exports = LMPUtils;
