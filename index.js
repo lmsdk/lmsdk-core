00e01a934bf2607e3f4e3cf1f11d82714d99634b
oblivioned <371397938@qq.com>
2019-11-22 15:01:57 +0800

    新增获取当前身份信息，包括地址，头像，昵称三个信息

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import core_language from './package/lmsdk-core-language'
 import core_theme from './package/lmsdk-core-theme'
 import core_assets from './package/lmsdk-core-assets'
 import core_dapps from './package/lmsdk-core-dapps'
+import core_utils from './package/lmsdk-core-utils'
 
 window.lmp = {
     navigator: new core_navigator(),
     language: new core_language(),
     theme: new core_theme(),
     assets: new core_assets(),
-    dapps: new core_dapps()
+    dapps: new core_dapps(),
+    utils: core_utils
 }
--- package/lmsdk-core-utils/index.js
+++ package/lmsdk-core-utils/index.js
@@ -1,9 +1,16 @@
 var LMPUtils = {}
 
 if (window.lmdapp) {
+    
     LMPUtils.inLmApp = true;
     LMPUtils.lmt = window.lmdapp.lmt;
-    LMPUtils.lmv = window.lmdapp.lmv;    
+    LMPUtils.lmv = window.lmdapp.lmv;
+
+    LMPUtils.identityInfo = function() {
+        return new Promise(function(resolve, reject) {
+            plus.bridge.exec("LMUtils", "getCuurrentUserInfo", [plus.bridge.callbackId(resolve, reject)])
+        });
+    }
 }
 
 module.exports = LMPUtils;
